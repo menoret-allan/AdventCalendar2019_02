1e83dd18215ec1c4b8bfbc9835cb0b061009362f
Allan Menoret <allan.menoret@gmail.com>
2019-12-04 11:05:13 +0100

    clean up a bit core

--- Core/Library.fs
+++ Core/Library.fs
@@ -1,10 +1,8 @@
 ﻿namespace Core
 
 type Instruction = 
-    | Error
     | End
-    | Add of (int * int)
-    | Mult of (int * int)
+    | Overide of (int * int)
 
 module Computer =
     let translate (instructions: string) =
@@ -14,12 +12,15 @@ module Computer =
     let translateBack (instructions: seq<int>) =
         instructions |> Seq.map(string) |> String.concat ","
 
+    let getValue (instructions: int []) pos =
+        instructions.[instructions.[pos]]
+
     let readInstruction (instructions: int []) (pos: int) =
         match instructions.[pos] with
-            | 99 -> End
-            | 1 -> Add(instructions.[instructions.[pos + 1]] + instructions.[instructions.[pos + 2]], instructions.[pos + 3])
-            | 2 -> Mult(instructions.[instructions.[pos + 1]] * instructions.[instructions.[pos + 2]], instructions.[pos + 3])
-            | _ -> Error
+            | 99 -> Some End
+            | 1 -> Some (Overide(getValue instructions (pos + 1) + getValue instructions (pos + 2), instructions.[pos + 3]))
+            | 2 -> Some (Overide(getValue instructions (pos + 1) * getValue instructions (pos + 2), instructions.[pos + 3]))
+            | _ -> None
 
     let update instructions pos value =
         Array.set instructions pos value
@@ -27,10 +28,9 @@ module Computer =
 
     let rec loop (instructions: int []) (pos: int) =
         match readInstruction instructions pos with
-        | End -> instructions
-        | Add (newValue, position)
-        | Mult (newValue, position) -> loop (update instructions position newValue) (pos+4)
-        | Error -> [|-1|]
+        | Some End -> instructions
+        | Some (Overide (newValue, position)) -> loop (update instructions position newValue) (pos+4)
+        | None -> [|-1|]
     
     let realCompute (instructions: seq<int>) =
         let array =  instructions |> Seq.toArray
@@ -45,16 +45,14 @@ module Computer =
         Array.set result 2 verb
         result
 
-
     let findSet (instructions: string) =
         let values = instructions |> translate |> Seq.toArray
         let pairs = seq {for noun in 0..100 do yield! seq {for verb in 0..100 do (noun , verb) } }
-        let rec testing (pair: (int * int)) =
+        let findSpecificResult (pair: (int * int)) =
                match loop (generateInstruction values pair) 0 |> Array.item 0 with
-               | 19690720 -> (true, pair)
-               | _ -> (false, pair)
-        let (_, pair) = pairs |> Seq.map(testing) |> Seq.filter(fun (is, pair) -> is) |> Seq.exactlyOne 
-        pair
+               | 19690720 -> Some pair
+               | _ -> None
+        pairs |> Seq.choose(findSpecificResult) |> Seq.exactlyOne 
 
 
 
