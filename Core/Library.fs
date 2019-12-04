3240d274881f350b499739206d5115c519d3b169
Allan Menoret <allan.menoret@gmail.com>
2019-12-04 02:00:27 +0100

    solution for the part 2

--- Core/Library.fs
+++ Core/Library.fs
@@ -1,6 +1,7 @@
 ﻿namespace Core
 
 type Instruction = 
+    | Error
     | End
     | Add of (int * int)
     | Mult of (int * int)
@@ -20,7 +21,7 @@ module Computer =
             | 99 -> End
             | 1 -> Add(instructions.[instructions.[pos + 1]] + instructions.[instructions.[pos + 2]], instructions.[pos + 3])
             | 2 -> Mult(instructions.[instructions.[pos + 1]] * instructions.[instructions.[pos + 2]], instructions.[pos + 3])
-            | _ -> raise (GrandMa("time to fix that"))
+            | _ -> Error
 
     let update instructions pos value =
         Array.set instructions pos value
@@ -31,6 +32,7 @@ module Computer =
         | End -> instructions
         | Add (newValue, position)
         | Mult (newValue, position) -> loop (update instructions position newValue) (pos+4)
+        | Error -> [|-1|]
     
     let realCompute (instructions: seq<int>) =
         let array =  instructions |> Seq.toArray
@@ -38,3 +40,32 @@ module Computer =
 
     let compute (instructions: string) =
         instructions |> translate |> realCompute |> translateBack
+
+    let generateInstruction instructions (noun, verb) =
+        let result=Array.copy instructions
+        Array.set result 1 noun
+        Array.set result 2 verb
+        result
+
+
+    let findSet (instructions: string) =
+        let values = instructions |> translate |> Seq.toArray
+        let pairs = seq {for noun in 0..100 do yield! seq {for verb in 0..100 do (noun , verb) } }
+        let rec testing (pair: (int * int)) =
+               match loop (generateInstruction values pair) 0 |> Array.item 0 with
+               | 19690720 -> (true, pair)
+               | _ -> (false, pair)
+        let (_, pair) = pairs |> Seq.map(testing) |> Seq.filter(fun (is, pair) -> is) |> Seq.exactlyOne 
+        pair
+
+
+
+
+
+
+
+
+
+
+
+
